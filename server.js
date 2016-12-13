2cf141c350979f81dd5dd32eeb68f114d093e2ed
lenkaB <lenka.bajcetic@gmail.com>
2016-12-13 20:25:16 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,48 @@ var express = require('express');
 var chance = require('chance').Chance();
 var app = express();
 
+
+
+
+
+        var chance = require('chance').Chance();
+    
+        const have = ['should', 'are supposed to', 'must', 'have to'];
+        const start = ['be busy', 'start', 'go back to', 'resume'];
+        const verb = ['working', 'typing', 'writing'];
+        const adjectives = ['energetically', 'eager', 'enthusiastic'];
+        const time = ['right now', 'instantly', 'right away', 'at once', 'straightaway'];
+
+        function choice(array) {
+            var index = chance.natural({
+                'min': 0,
+                'max': array.length - 1
+            });
+            return array[index];
+
+        }
+
+        function maybe(array) {
+            if (chance.bool()) {
+                return choice(array);
+            } else {
+                return '';
+            }
+        }
+
+        function procrast() {
+            var message = 'Hi! You ' + choice(have) + ' ' + choice(start) + ' ' + choice(verb) + ' ' + maybe(adjectives) + ' ' + choice(time) + '. ';
+            return message;
+
+        }
+
+
+
+
+
+
+
+
 //setting middleware
 app.use(express.static(__dirname + "/public")); //Serves resources from public folder
 
@@ -34,3 +76,16 @@ app.get('/', function (req, res) {
 server.listen(8088, function () {
     console.log('listening on port: ' + 8088);
 });
+
+
+io.on('connection', function(socket) {
+
+console.log('got a connection');
+ socket.on('procrast', function() {
+
+    // send back message
+        msg = procrast();
+  	io.emit('message', msg);
+
+  });});
+
